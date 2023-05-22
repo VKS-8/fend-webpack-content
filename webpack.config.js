cfd9afb29de1e47094fb11fa0a866fcb447123ad
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-05-21 20:41:29 -0500

    Update  index.js in the server to use dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html"
+            filename: "./index.html",
+            inject: true
         })
     ]
 }
